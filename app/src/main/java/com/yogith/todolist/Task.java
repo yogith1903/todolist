e2dc3d046c7982c2a10d748978179110a6b7e295
yogith1903 <yogith1220@gmail.com>
2026-08-30 21:49:12 +0530

    This our model class

--- app/src/main/java/com/yogith/todolist/Task.java
+++ app/src/main/java/com/yogith/todolist/Task.java
@@ -1,4 +1,24 @@
 package com.yogith.todolist;
 
 public class Task {
+
+    private String taskName;
+    private boolean completed;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.completed = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
