2e3a2b6a487b266a889538595ae3bc6d1e156c88
yogith1903 <yogith1220@gmail.com>
2026-08-30 21:53:40 +0530

    To display task in recycleView

--- app/src/main/java/com/yogith/todolist/TaskAdapter.java
+++ app/src/main/java/com/yogith/todolist/TaskAdapter.java
@@ -1,4 +1,75 @@
 package com.yogith.todolist;
 
-public class TaskAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    private ArrayList<Task> taskList;
+
+    public TaskAdapter(ArrayList<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_item, parent, false);
+
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull TaskViewHolder holder,
+            int position) {
+
+        Task task = taskList.get(position);
+
+        holder.taskCheckBox.setText(task.getTaskName());
+
+        holder.taskCheckBox.setChecked(task.isCompleted());
+
+        holder.taskCheckBox.setOnCheckedChangeListener(
+                (buttonView, isChecked) -> {
+                    task.setCompleted(isChecked);
+                }
+        );
+
+        holder.deleteButton.setOnClickListener(v -> {
+
+            taskList.remove(position);
+
+            notifyItemRemoved(position);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    static class TaskViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox taskCheckBox;
+        Button deleteButton;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            taskCheckBox = itemView.findViewById(R.id.taskCheckBox);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
+        }
+    }
 }
